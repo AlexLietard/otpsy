42b31ac888b6174018c7250380a7cc994851b63d
Alexandre Lietard <alex.lietard77@gmail.com>
2023-12-27 14:51:15 +0100

    add threshold to each trace

--- OTPsy/src/visualise/app.py
+++ OTPsy/src/visualise/app.py
@@ -42,7 +42,6 @@ CHECKLIST_DISTANCE = {
 }
 
 def main(df, column_to_vis):
-
     app = dash.Dash(__name__, external_stylesheets=[
                     dbc.themes.DARKLY])  # set app layout
     list_of_method = ["IQR", "MAD", "SD", "rSD"]
@@ -61,14 +60,14 @@ def main(df, column_to_vis):
                            'padding-right': "0.5rem"}), 
                            'value': num})
 
-    fig = px.scatter(df,
-                     x=df.reset_index().index,
-                     y=column_to_vis[0],
-                     hover_name=df.index)  # initialize app
+    fig = px.scatter({'data':[]})  # initialize app
     sidebar = html.Div(
                     [dbc.Col(
                         html.Div([
                             html.H4("Options"),
+                            # in this column, there is 3 row, starting with a line,
+                            # followed by the name of the category and options
+                            # Columns to show
                                 dbc.Row(html.Div([
                                     html.Hr(),
                                     html.P(
@@ -76,11 +75,11 @@ def main(df, column_to_vis):
                                     dcc.Dropdown(
                                         options=[{'label': i, 'value': i}
                                                 for i in column_to_vis],
-                                        value=str(column_to_vis[0]),
                                         id='column',
                                         className = "drop",
                                         clearable=False, multi=True
                                 )])),
+                                # Method to use
                                 dbc.Row(
                                     html.Div([
                                         html.Hr(),
@@ -94,6 +93,7 @@ def main(df, column_to_vis):
                                                 "backgroundColor": "#324c71",
                                                 "borderColor": "#324c71"}
                                 )])),
+                                # Distance to use
                                 dbc.Row(
                                     html.Div([
                                         html.Hr(),
@@ -101,7 +101,6 @@ def main(df, column_to_vis):
                                             "Select distance"
                                         ),
                                         dbc.Checklist(options=ls_distance_with_html,
-                                            value = [2],
                                             id="distance",
                                             style = CHECKLIST_DISTANCE
                                 )]))
@@ -127,68 +126,90 @@ def main(df, column_to_vis):
     )
     def update_graph(y, method, distance):
         triggered_id = dash.ctx.triggered_id
+        # allow to know which subplot corresponds to the method
+        ref = {}
+        if y == None or len(y) == 0:
+            fig = px.scatter({'data': []})
+            # number_of_subplots = 1 because the value of number_of_subplots 
+            # is used to increase the height_of_figure containing the subplot.
+            # I increase the height_of_figure for each subplot added
+            # with : height_of_figure = height_of_a_subplot * number_of_subplots. 
+            # However, if there is no column (y == None), height_of_figure can't be 0.
+            number_of_subplots = 1
+        else:
+            title_of_subplots = [f"Scatter plot of {col}" for col in y]
+            fig = make_subplots(rows = len(y), 
+                                cols= 1, 
+                                subplot_titles=title_of_subplots)
+            for i, column in enumerate(y):
+                fig.add_trace(
+                    go.Scatter(x = df.reset_index().index, 
+                            y=df[column],
+                            hovertext=df.index,
+                            mode="markers",
+                            xaxis=f"x{i+1}",
+                            yaxis=f"y{i+1}"),
+                            row=i+1, col=1)
+                fig.update_xaxes(title="Row Number")
+                fig.update_yaxes(title=column)
+                ref[column] = i + 1
+            
+            fig = update_threshold(df, fig, method, y, distance, ref)
 
-        # Debug
-        print("Y", y)
-        print("Method :", method)
-        print("Distance : ", distance)
-        print(df[y[0]])
-        fig = make_subplots(rows = len(y), cols= 1)
-        for i in range(len(y)):
-            fig.add_trace(
-                go.Scatter(x = df.reset_index().index, 
-                           y=df[y[i]],
-                           hovertext=df.index, mode="markers",
-                           xaxis=f"x{i+1}",
-                           yaxis=f"y{i+1}"),
-                           row=i+1, col=1)
-        
-        if triggered_id == 'method' or triggered_id == "distance":
-            fig = update_threshold(df, fig, method, y, distance)
+            number_of_subplots = i + 1
+        height_of_one_plot = 400
         
         fig.update_layout(
+            height = height_of_one_plot*number_of_subplots,
+            width = 900,
             margin=dict(l=30, r=30, t=30, b=20),
         )
         return fig
 
     app.run_server(debug=True)
 
-def update_threshold(df, fig, pre_method, y, distance):
+def update_threshold(df, fig, pre_method, y, distance, ref):
     layout = {"showlegend":True}
     layout['shapes'] = []
     len_df = len(df.index)
-    for met in pre_method:
-        method = met.lower()
-        func = config.DICT_FUNCTION[method]
-        # give a default value when it is not input
-        if distance == None:
-            distance = [2]
-        for dis in distance:
-            if method == "mad":
-                low_threshold, high_threshold = func(
-                    df, [y], dis)
-            else:
-                low_threshold, high_threshold = func(
-                    df, [y], dis)
-            low_line = create_line_plotly(low_threshold, len_df, method)
-            layout['shapes'].append(low_line)
-            high_line = create_line_plotly(high_threshold, len_df, method)
-            layout['shapes'].append(high_line)
+    if pre_method == None or distance == None:
+        return fig
+    for column in y:
+        for met in pre_method:
+            method = met.lower()
+            func = config.DICT_FUNCTION[method]
+            # give a default value when it is not input
+            if distance == None:
+                distance = [2]
+            for dis in distance:
+                if method == "mad":
+                    low_threshold, high_threshold = func(
+                        df, [column], dis)
+                else:
+                    low_threshold, high_threshold = func(
+                        df, [column], dis)
+                low_line = create_line_plotly(low_threshold, len_df, method, ref[column])
+                layout['shapes'].append(low_line)
+                high_line = create_line_plotly(high_threshold, len_df, method, ref[column])
+                layout['shapes'].append(high_line)
 
     fig.update_layout(layout)
 
     return fig
 
 
-def create_line_plotly(threshold, len_df, method):
+def create_line_plotly(threshold, len_df, method, reference):
     line={
         'type': 'line',
-           'x0': 0,
-           'y0': threshold,
-           'x1': len_df,
-           'y1': threshold,
-           'line': {'color': f'{config.HEXA_FOR_PLOTLY[method]}','width': 4},
-        }
+        'xref': f'x{reference}',
+        'yref': f'y{reference}',
+        'x0': 0,
+        'y0': threshold,
+        'x1': len_df,
+        'y1': threshold,
+        'line': {'color': f'{config.HEXA_FOR_PLOTLY[method]}','width': 4},
+    }
+    print(line)
     return line
 
 

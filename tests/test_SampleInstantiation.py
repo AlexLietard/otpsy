981f554638809dc8afef53414f781777af152ba6
Alexandre Lietard <alex.lietard77@gmail.com>
2024-02-17 22:40:50 +0100

    instantionSample test started

---
+++ tests/test_SampleInstantiation.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os 
+import pytest
+df = pd.read_csv("data.csv", sep=";")
+
+# Allow to have the local copy of package
+os.chdir("../src")
+import otpsy as ot
+
+@pytest.mark.parametrize("df", [df])
+class TestClass:
+    def test_sample_instantiation(self, df):
+        sample_two_cols = \
+            ot.Sample(df[["age", "random_col"]])
+        
+        sample_columns_to_test = \
+            ot.Sample(df, columns_to_test="age")
+        
+        sample_columns_to_test = \
+            ot.Sample(df, columns_to_test=["age", "random_col"])
+        
+        sample_columns_to_test_number_unique = \
+            ot.Sample(df, columns_to_test=3)
+        
+        sample_columns_to_test_number = \
+            ot.Sample(df, columns_to_test=[3, 4])         
+        
+        sample_columns_to_test_p_col = \
+            ot.Sample(df, columns_to_test=["age", "random_col"], participant_column="index_participant")
+
+        sample_columns_to_test_p_col = \
+            ot.Sample(df, columns_to_test=["age", "random_col"], participant_column=1)
